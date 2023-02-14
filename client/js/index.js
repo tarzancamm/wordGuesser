a1594fd9cb5864004f4b165f6dd5f9bcef27ec3b
Cameron Miner <cameronsminer@gmail.com>
2023-02-14 12:48:01 -0700

    Finished game

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,14 @@
+const inputs = document.querySelector(".inputs")
+const hint = document.querySelector(".hint span")
+const guessesLeft = document.querySelector(".guess-left span")
+const wrongLetters = document.querySelector(".wrong-letter span")
+const resetBtn = document.querySelector(".reset-btn")
+const typingInput = document.querySelector(".typing-input")
+
+let word
+let maxGuesses
+let incorrectLetters = []
+let correctLetters = []
 let wordList = [
     {
         word: "python",
@@ -196,3 +207,64 @@ let wordList = [
         hint: "related to a water"
     },
 ]
+
+const randomWord = () => {
+    let randomItem = wordList[Math.floor(Math.random() * wordList.length)]
+    word = randomItem.word
+    maxGuesses = word.length >= 5 ? 8 : 6;
+    correctLetters = []
+    incorrectLetters = []
+
+    // innerHTML: print content between labels, including text. Considers spaces. Returns an inner element tag. Use innerText if NOT trying to add HTML elements.
+    // innerText: prints plain text between tags and ignores space.
+    // textContent: BEST PRACTICE newer version of innerText. Some browsers only support old style, innerText.
+    hint.textContent = randomItem.hint
+    guessesLeft.textContent = maxGuesses
+    wrongLetters.textContent = incorrectLetters
+
+    let html = ""
+    for (let i = 0; i < word.length; i++) {
+        html += '<input type="text" disabled >'
+        inputs.innerHTML = html
+    }
+}
+
+const initGame = (e) => {
+    let key = e.target.value.toLowerCase()
+    if(key.match(/^[A-Za-z]+$/) && !incorrectLetters.includes(key) && !correctLetters.includes(key)) {
+        if(word.includes(key)) {
+            for (let i = 0; i < word.length; i++) {
+                if(word[i] == key) {
+                    correctLetters += key
+                    inputs.querySelectorAll("input")[i].value = key
+                }
+            }
+        } else {
+            maxGuesses--
+            incorrectLetters.push(`${key}`)
+        }
+        guessesLeft.innerText = maxGuesses
+        wrongLetters.innerText = incorrectLetters
+    }
+    typingInput.value = ""
+
+    setTimeout(() => {
+        if(correctLetters.length === word.length) {
+            alert(`Congrats! You found the word ${word.toUpperCase()}`);
+            return randomWord();
+        } else if(maxGuesses < 1) {
+            alert("Game over! You have run out of guesses");
+            for(let i = 0; i < word.length; i++) {
+                inputs.querySelectorAll("input")[i].value = word[i];
+            }
+        }
+    }, 100);
+}
+
+// Functions to run on initialization
+randomWord()
+
+resetBtn.addEventListener('click', randomWord)
+typingInput.addEventListener('input', initGame)
+inputs.addEventListener('click', () => typingInput.focus())
+document.addEventListener("keydown", () => typingInput.focus())
